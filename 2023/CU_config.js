8ab36c3492c53043bd063fb0d7e64a75e46bef69
billyparmenter <bparmenter6387@conestogac.on.ca>
2023-01-29 00:37:17 -0500

    Revert "removed tba"
    This reverts commit 8560d4eb6ce94cc7f058d19313da4adcfa5a75f4.

--- 2023/CU_config.js
+++ 2023/CU_config.js
@@ -23,7 +23,8 @@ var config_data = `
       "type": "level",
       "choices": {
         "qm": "Quals<br>",
-        "de": "Double Elimination<br>"
+        "de": "Double Elimination<br>",
+        "f": "Finals"
       },
       "defaultValue": "qm",
       "required": "true"
@@ -34,6 +35,19 @@ var config_data = `
       "min": 1,
       "max": 100,
       "required": "true"
+    },
+    { "name": "Robot",
+      "code": "r",
+      "type": "robot",
+      "choices": {
+        "r1": "Red-1",
+        "b1": "Blue-1<br>",
+        "r2": "Red-2",
+        "b2": "Blue-2<br>",
+        "r3": "Red-3",
+        "b3": "Blue-3"
+      },
+      "required":"true"
     },
     { "name": "Team #",
       "code": "t",
