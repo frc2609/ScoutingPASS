5b4253530621a59eaa4d5f9898eab3cbb4f68c83
billyParmenter <billyparmenter98@gmail.com>
2023-03-24 08:58:30 -0400

    comment page

---
+++ 2023/CU_Comment_config.js
@@ -0,0 +1,33 @@
+var config_data = `
+{
+  "title": "Scouting PASS 2023",
+  "page_title": "Charged Up",
+  "pitConfig": "true",
+  "prematch": [
+    { "name": "Event",
+      "code": "e",
+      "type": "event",
+      "defaultValue": "2023onwat",
+      "required": "true",
+      "disabled": "true"
+    },
+    { "name": "Team Number",
+      "code": "t",
+      "type": "number"
+    },
+    { "name": "Comments",
+      "code": "com",
+      "type": "text",
+      "size": 20,
+      "maxSize": 500
+    }
+  ],
+  "auton": [
+  ],
+  "teleop": [
+  ],
+  "endgame": [
+  ],
+  "postmatch": [
+  ]
+}`;
