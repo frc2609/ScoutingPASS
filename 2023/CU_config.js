ccedf0cf26c742b98002befe9f09e58efd36cb5e
billyparmenter <bparmenter6387@conestogac.on.ca>
2023-03-29 00:33:04 -0400

    set event to testing

--- 2023/CU_config.js
+++ 2023/CU_config.js
@@ -14,7 +14,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2023onham",
+      "defaultValue": "testing",
       "required": "true",
       "disabled": "true"
     },
