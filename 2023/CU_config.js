c695ef847bba21b5431c9ef02b9f79f65286d3ba
billyparmenter <bparmenter6387@conestogac.on.ca>
2023-04-05 22:56:02 -0400

    changed event

--- 2023/CU_config.js
+++ 2023/CU_config.js
@@ -14,7 +14,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2023onham",
+      "defaultValue": "2023oncmp2",
       "required": "true",
       "disabled": "true"
     },
